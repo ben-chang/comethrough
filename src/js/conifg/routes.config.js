6d4ff3bb398c097d4067c5bf42b6bd4faecc1f11
Benjamin Chang <ben.chang@hotmail.co.uk>
2017-09-27 15:01:47 -0700

    added router, views

---
+++ src/js/conifg/routes.config.js
@@ -0,0 +1,29 @@
+angular.module('comethrough')
+  .config(Router);
+
+Router.$inject = ['$stateProvider', '$locationProvider', '$urlRouterProvider'];
+function Router($stateProvider, $locationProvider, $urlRouterProvider) {
+  $locationProvider.html5Mode(true);
+
+  $stateProvider
+    .state('splash', {
+      url: '/',
+      templateUrl: '/js/views/splash.html'
+      // controller: 'SplashCtrl',
+      // controllerAs: 'splash'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: '/js/views/auth/register.html'
+      // controller: 'RegisterCtrl',
+      // controllerAs: 'register'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: '/js/views/auth/login.html'
+      // controller: 'LoginCtrl',
+      // controllerAs: 'login'
+    });
+
+  $urlRouterProvider.otherwise('/');
+}
